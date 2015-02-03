8cf8d81cfa6e071237d2fc51e8de3f608ab14cbf
deborahhellen <hellen.deborahv@gmail.com>
2015-02-03 16:29:33 -0500

    Working on hw4

--- classes/class4/homework4/public/javascripts/main.js
+++ classes/class4/homework4/public/javascripts/main.js
@@ -1,8 +1,11 @@
 //Handles the ajax requests on the /ingredients page
 
 $(document).ready(function() { 
+
 	var $addForm = $(".addForm");
 	var $outOfStockForm = $(".outOfStockForm");
+	var $editForm = $(".editForm");
+
 	var onError = function(data, status) {
 		//Log the status and error to the console
 		//This will be generic error generator for my ajax requests
@@ -13,16 +16,20 @@ $(document).ready(function() {
 	var onSuccessAdd = function(data, status) {
 		//Reloads the section when we've successfully added the item
 		//TODO: Figure out how to load just the list item instead of the whole page
-		window.location.reload();
+		//window.location.reload();
+		console.log("Successfully added " + data);
 	};
 
 	var onSuccessDel = function(data, status) {
 		//Reloads the section when we've successfully deleted the item
-		window.location.reload();
+		//window.location.reload();
+		console.log("Successfully deleted" + data);
 	};
 
 	var onSuccssEdit = function(data, status) {
 		//Reloads the window when we've successfully edited the item
+		//window.location.reload();
+		console.log("Successfully edited " + data);
 	};
 
 	$addForm.submit(function(event) {
@@ -47,6 +54,21 @@ $(document).ready(function() {
 			id: id
 		})
 		.done(onSuccessDel)
+		.error(onError);
+	});
+
+	$editForm.submit(function(event) {
+		event.preventDefault();
+		var name = $editForm.find("[name = 'name']").val();
+		var price = $editForm.find("[name = 'price']").val();
+		var id = $editForm.find("[name = 'id']").val();
+
+		$.post('edit', {
+			name: name,
+			price: price,
+			id: id
+		})
+		.done(onSuccssEdit)
 		.error(onError);
 	});
 });
